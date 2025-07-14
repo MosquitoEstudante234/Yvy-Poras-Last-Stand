5ff1f591506a72a7db6dfeccceca2ee4ca29b7fa
SolDeBotas <mosquitoestudante34234@gmail.com>
2025-07-14 19:48:06 -0300

    NPC para interagir com o Canva

---
+++ Yvy Porã’s Last Stand/Assets/_Scripts/UIScripts/NPCInteraction.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class NPCInteraction : MonoBehaviour
+{
+    public GameObject npcCanvas; // arraste o Canvas do NPC aqui no inspetor
+    private bool playerInside = false;
+
+    void Start()
+    {
+        if (npcCanvas != null)
+            npcCanvas.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (playerInside && Input.GetKeyDown(KeyCode.E))
+        {
+            ToggleCanvas(true);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = false;
+            ToggleCanvas(false);
+        }
+    }
+
+    void ToggleCanvas(bool state)
+    {
+        if (npcCanvas != null)
+        {
+            npcCanvas.SetActive(state);
+            Cursor.visible = state;
+            Cursor.lockState = state ? CursorLockMode.None : CursorLockMode.Locked;
+        }
+    }
+}
